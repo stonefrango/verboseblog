c5f695b7de374d0ec4498ec94637d73b7642eefe
Stone Frangowlakis <stonefrango+github@gmail.com>
2025-07-11 15:20:12 -0400

    aaaaaaa

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,4 +28,4 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-DEFAULT_METADATA = {'<meta name="fediverse:creator" content="@stonefrangowlakis@mastodon.social" />'}
+DEFAULT_METADATA = {"fediverse:creator": "@stonefrangowlakis@mastodon.social"}
